60d6ce1d59234d6d096006e0cc51c9e44963e657
David Amaro <dav1494@ciencias.unam.mx>
2016-08-24 14:38:39 -0500

    Se agrega println para ver si se muestra en la consola de Travis.

--- test/avion.jl
+++ test/avion.jl
@@ -3,6 +3,10 @@ using Base.Test
 const RoundNDigits = RoundingMode{:RoundNDigits}()
 Base.round(n::Number, digits::Int, ::RoundingMode{:RoundNDigits}) = round(n, -round(Int,floor(log10(abs(n)))) + (digits - 1))
 
+println("Este programa es una prueba para los demás de cómo funciona la vida")
+corinal = 1.123e12
+cnueva = 1.0e12
+println("Cifra original: $corinal. Cifra nueva: $cnueva.")
 @test round(1.123e12, 1, RoundNDigits) == 1.0e12
 @test round(1.123e12, 2, RoundNDigits) == 1.1e12
 @test round(1.123e12, 3, RoundNDigits) == 1.12e12
